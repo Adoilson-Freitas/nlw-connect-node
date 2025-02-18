39d13d81dac8131a4ed2cdac1a087f99a88631f1
Adoilson-Freitas <adoilsonfreitas@gmail.com>
2025-02-18 13:06:47 -0300

    feat: add subscription route with validation and config swagger

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,41 @@
-import { fastify } from 'fastify'
-import { fastifyCors } from '@fastify/cors'
-import { validatorCompiler, serializerCompiler} from "fastify-type-provider-zod"
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  ZodTypeProvider,
+  jsonSchemaTransform
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger"
+import { fastifySwaggerUi } from "@fastify/swagger-ui"
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
-const app = fastify()
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifyCors, {
-  origin: true
+  origin: true,
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "NLW Connect",
+      version: "0.0.1",
+    },
+  }, 
+  transform: jsonSchemaTransform
 })
 
-app.listen({port: 3333}).then(() => {
-  console.log('Server is running on port 3333')
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
 })
+
+app.register(subscribeToEventRoute)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server is running on port 3333");
+});
